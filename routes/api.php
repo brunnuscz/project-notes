92a3a49fe633b01f74102187474b87ced00bdc62
brunnuscz <brunocruz012013@gmail.com>
2026-06-08 11:28:44 -0300

    Feat: create new route.

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('notes')->name('notes.')->group(function () {
     Route::get('/', [NoteApiController::class, 'index'])->name('index');
+    Route::get('/trash', [NoteApiController::class, 'trash'])->name('trash');
     Route::post('/', [NoteApiController::class, 'store'])->name('store');
     Route::get('/search', [NoteApiController::class, 'search'])->name('search');
     Route::get('{id}', [NoteApiController::class, 'show'])->name('show');
